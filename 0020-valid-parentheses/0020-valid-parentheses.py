d569c357bff9e6ea386fca93456571ab6c26b299
chltnqud0 <50440687+chltnqud0@users.noreply.github.com>
2024-05-19 17:19:19 +0900

    Time: 41 ms (19.46%), Space: 16.7 MB (22.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        list_s = list(s)
+        dict_match = {"{": "}", "[": "]", "(": ")"}
+        list_temp = list()
+        while list_s:
+            temp = list_s.pop()
+            if temp in dict_match:
+                if not list_temp:
+                    return False
+                if dict_match[temp] != list_temp.pop():
+                    return False
+            else:
+                list_temp.append(temp)
+        if not list_temp:
+            return True
