e8dcf98b056d617ca378182697415fe13e9ffd97
chltnqud0 <50440687+chltnqud0@users.noreply.github.com>
2024-05-22 13:59:19 +0900

    Time: 341 ms (19.3%), Space: 149 MB (47.37%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.dart
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  TreeNode? invertTree(TreeNode? root) {
+    if (root == null) {
+        return null;
+    }
+    TreeNode? temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+    return root;
+  }
+}
